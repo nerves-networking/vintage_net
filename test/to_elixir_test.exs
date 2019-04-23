0c085cedc3684b93afa7e48bf3246744a7c953ef
Frank Hunleth <fhunleth@troodon-software.com>
2019-04-23 15:56:32 -0400

    fix udhcpc tests

--- test/to_elixir_test.exs
+++ test/to_elixir_test.exs
@@ -11,72 +11,104 @@ defmodule ToElixirTest do
   end
 
   test "udhcpc handler notifies Elixir" do
-    assert capture_log(fn ->
-             udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
+    log =
+      capture_log(fn ->
+        udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
 
-             System.cmd(udhcpc_handler, ["deconfig"],
-               env: [
-                 {"subnet", "255.255.255.0"},
-                 {"router", "192.168.9.1"},
-                 {"opt58", "0000a8c0"},
-                 {"opt59", "00012750"},
-                 {"domain", "example.net"},
-                 {"interface", "eth0"},
-                 {"siaddr", "192.168.9.1"},
-                 {"dns", "192.168.9.1"},
-                 {"serverid", "192.168.9.1"},
-                 {"broadcast", "192.168.9.255"},
-                 {"ip", "192.168.9.131"},
-                 {"mask", "24"},
-                 {"lease", "86400"},
-                 {"opt53", "05"}
-               ]
-             )
+        System.cmd(udhcpc_handler, ["deconfig"],
+          env: [
+            {"subnet", "255.255.255.0"},
+            {"router", "192.168.9.1"},
+            {"opt58", "0000a8c0"},
+            {"opt59", "00012750"},
+            {"domain", "example.net"},
+            {"interface", "eth0"},
+            {"siaddr", "192.168.9.1"},
+            {"dns", "192.168.9.1"},
+            {"serverid", "192.168.9.1"},
+            {"broadcast", "192.168.9.255"},
+            {"ip", "192.168.9.131"},
+            {"mask", "24"},
+            {"lease", "86400"},
+            {"opt53", "05"}
+          ]
+        )
 
-             Process.sleep(250)
-           end) =~
-             "[debug] udhcpc.deconfig(eth0): %{broadcast: \"192.168.9.255\", command: :deconfig, dns: [\"192.168.9.1\"], domain: \"example.net\", interface: \"eth0\", ip: \"192.168.9.131\", lease: \"86400\", mask: \"24\", opt53: \"05\", opt58: \"0000a8c0\", opt59: \"00012750\", router: [\"192.168.9.1\"], serverid: \"192.168.9.1\", siaddr: \"192.168.9.1\", subnet: \"255.255.255.0\"}"
+        Process.sleep(250)
+      end)
+
+    assert log =~ "broadcast: \"192.168.9.255\""
+    assert log =~ "command: :deconfig"
+    assert log =~ "dns: [\"192.168.9.1\"]"
+    assert log =~ "domain: \"example.net\""
+    assert log =~ "interface: \"eth0\""
+    assert log =~ "ip: \"192.168.9.131\""
+    assert log =~ "lease: \"86400\""
+    assert log =~ "mask: \"24\""
+    assert log =~ "opt53: \"05\""
+    assert log =~ "opt58: \"0000a8c0\""
+    assert log =~ "opt59: \"00012750\""
+    assert log =~ "router: [\"192.168.9.1\"]"
+    assert log =~ "serverid: \"192.168.9.1\""
+    assert log =~ "siaddr: \"192.168.9.1\""
+    assert log =~ "subnet: \"255.255.255.0\""
   end
 
   test "udhcpc handler ignores uppercase fields" do
-    assert capture_log(fn ->
-             udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
+    log =
+      capture_log(fn ->
+        udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
 
-             System.cmd(udhcpc_handler, ["deconfig"],
-               env: [
-                 {"interface", "eth0"},
-                 {"broadcast", "broadcast"},
-                 {"subnet", "subnet"},
-                 {"router", "router"},
-                 {"domain", "domain"},
-                 {"PATH", "stuff"},
-                 {"EMU", "beam"}
-               ]
-             )
+        System.cmd(udhcpc_handler, ["deconfig"],
+          env: [
+            {"interface", "eth0"},
+            {"broadcast", "broadcast"},
+            {"subnet", "subnet"},
+            {"router", "router"},
+            {"domain", "domain"},
+            {"PATH", "stuff"},
+            {"EMU", "beam"}
+          ]
+        )
 
-             Process.sleep(250)
-           end) =~
-             "[debug] udhcpc.deconfig(eth0): %{broadcast: \"broadcast\", command: :deconfig, domain: \"domain\", interface: \"eth0\", router: [\"router\"], subnet: \"subnet\"}"
+        Process.sleep(250)
+      end)
+
+    assert log =~ "broadcast: \"broadcast\""
+    assert log =~ "command: :deconfig"
+    assert log =~ "domain: \"domain\""
+    assert log =~ "interface: \"eth0\""
+    assert log =~ "router: [\"router\"]"
+    assert log =~ "subnet: \"subnet\""
   end
 
   test "udhcpc handler handles multiple dns" do
-    assert capture_log(fn ->
-             udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
+    log =
+      capture_log(fn ->
+        udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
 
-             System.cmd(udhcpc_handler, ["deconfig"],
-               env: [
-                 {"interface", "eth0"},
-                 {"ip", "ip"},
-                 {"broadcast", "broadcast"},
-                 {"subnet", "subnet"},
-                 {"domain", "domain"},
-                 {"dns", "1.1.1.1 2.2.2.2 3.3.3.3 4.4.4.4"},
-                 {"message", "message"}
-               ]
-             )
+        System.cmd(udhcpc_handler, ["deconfig"],
+          env: [
+            {"interface", "eth0"},
+            {"ip", "ip"},
+            {"broadcast", "broadcast"},
+            {"subnet", "subnet"},
+            {"domain", "domain"},
+            {"dns", "1.1.1.1 2.2.2.2 3.3.3.3 4.4.4.4"},
+            {"message", "message"}
+          ]
+        )
 
-             Process.sleep(250)
-           end) =~
-             "[debug] udhcpc.deconfig(eth0): %{broadcast: \"broadcast\", command: :deconfig, dns: [\"1.1.1.1\", \"2.2.2.2\", \"3.3.3.3\", \"4.4.4.4\"], domain: \"domain\", interface: \"eth0\", ip: \"ip\", message: \"message\", subnet: \"subnet\"}"
+        Process.sleep(250)
+      end)
+
+    assert log =~ "broadcast: \"broadcast\""
+    assert log =~ "command: :deconfig"
+    assert log =~ "dns: [\"1.1.1.1\", \"2.2.2.2\", \"3.3.3.3\", \"4.4.4.4\"]"
+    assert log =~ "domain: \"domain\""
+    assert log =~ "interface: \"eth0\""
+    assert log =~ "ip: \"ip\""
+    assert log =~ "message: \"message\""
+    assert log =~ "subnet: \"subnet\""
   end
 end
