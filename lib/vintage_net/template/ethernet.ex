8c0df0c24335e316dbbbb0d500b482a717ab5799
Frank Hunleth <fhunleth@troodon-software.com>
2019-05-14 08:42:31 -0400

    Remove unused configuration templates
    Since we keep a cookbook of configs in the README.md, these seem less
    useful. Most importantly, they weren't implemented. We can come back.

--- lib/vintage_net/template/ethernet.ex
+++
@@ -1,9 +0,0 @@
-defmodule VintageNet.Template.Ethernet do
-  @moduledoc """
-  Settings templates for wired Ethernet connections
-  """
-
-  def simple_dhcp() do
-    %{ipv4: %{method: :dhcp}}
-  end
-end
--- lib/vintage_net/template/mobile.ex
+++
@@ -1,5 +0,0 @@
-defmodule VintageNet.Template.Cellular do
-  @moduledoc """
-  Settings templates for cellular connections
-  """
-end
