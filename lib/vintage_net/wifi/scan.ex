bd392bbbd275602655e73c7a2d652d7799215f0d
Connor Rigby <konnorrigby@gmail.com>
2019-05-30 15:58:39 -0700

    Add more flags for parsing mesh, eap and rsn-ccmp networks

--- lib/vintage_net/wifi/scan.ex
+++ lib/vintage_net/wifi/scan.ex
@@ -73,8 +73,12 @@ defmodule VintageNet.WiFi.Scan do
   end
 
   defp parse_flag("WPA2-PSK-CCMP"), do: [:wpa2_psk_ccmp]
+  defp parse_flag("WPA2-EAP-CCMP"), do: [:wpa2_eap_ccmp]
+  defp parse_flag("IBSS"), do: [:ibss]
+  defp parse_flag("MESH"), do: [:mesh]
   defp parse_flag("ESS"), do: [:ess]
   defp parse_flag("P2P"), do: [:p2p]
+  defp parse_flag("RSN--CCMP"), do: [:rsn_ccmp]
 
   defp parse_flag(other) do
     _ = Logger.warn("Ignoring unknown WiFi Access Point flag: #{other}")
--- test/vintage_net/wifi/scan_test.exs
+++ test/vintage_net/wifi/scan_test.exs
@@ -30,4 +30,61 @@ defmodule VintageNet.WiFi.ScanTest do
 
     assert output == Scan.parse(results)
   end
+
+  test "wpa2 eap access point" do
+    results = """
+    bssid / frequency / signal level / flags / ssid
+    ac:86:74:5b:ea:42\t2462\t-81\t[WPA2-EAP-CCMP][ESS]\tenterprise lan
+    """
+
+    output = [
+      %VintageNet.WiFi.AccessPoint{
+        bssid: "ac:86:74:5b:ea:42",
+        frequency: 2462,
+        signal: -81,
+        flags: [:wpa2_eap_ccmp, :ess],
+        ssid: "enterprise lan"
+      }
+    ]
+
+    assert output == Scan.parse(results)
+  end
+
+  test "mesh access point" do
+    results = """
+    bssid / frequency / signal level / flags / ssid
+    ae:86:74:5b:ea:46\t2462\t-69\t[RSN--CCMP][MESH]\tmesh
+    """
+
+    output = [
+      %VintageNet.WiFi.AccessPoint{
+        bssid: "ae:86:74:5b:ea:46",
+        frequency: 2462,
+        signal: -69,
+        flags: [:rsn_ccmp, :mesh],
+        ssid: "mesh"
+      }
+    ]
+
+    assert output == Scan.parse(results)
+  end
+
+  test "ibss access point" do
+    results = """
+    bssid / frequency / signal level / flags / ssid
+    26:9e:db:0d:4f:21\t2412\t-74\t[IBSS]\tSETUP
+    """
+
+    output = [
+      %VintageNet.WiFi.AccessPoint{
+        bssid: "26:9e:db:0d:4f:21",
+        frequency: 2412,
+        signal: -74,
+        flags: [:ibss],
+        ssid: "SETUP"
+      }
+    ]
+
+    assert output == Scan.parse(results)
+  end
 end
