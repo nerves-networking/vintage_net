0ae172e41a343e14fda07f25e5475c1dd514cfad
Frank Hunleth <fhunleth@troodon-software.com>
2019-06-03 16:11:29 -0400

    Skip tests on OSX that require InterfacesMonitor

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,6 @@
 File.rm_rf!("test/tmp")
-ExUnit.start()
+
+# VintageNet.InterfacesMonitor only works on Linux
+exclude = if :os.type() == {:unix, :linux}, do: [], else: [requires_interfaces_monitor: true]
+
+ExUnit.start(exclude: exclude)
