3d2df28cca4cd764627917d07080d0535df91c2e
Frank Hunleth <fhunleth@troodon-software.com>
2019-06-16 12:36:00 -0400

    Add initial implementation of WiFi scanning support

--- lib/vintage_net/wifi/wpa_supplicant.ex
+++ lib/vintage_net/wifi/wpa_supplicant.ex
@@ -1,7 +1,7 @@
 defmodule VintageNet.WiFi.WPASupplicant do
   use GenServer
 
-  alias VintageNet.WiFi.WPASupplicantLL
+  alias VintageNet.WiFi.{WPASupplicantDecoder, WPASupplicantLL}
   require Logger
 
   @moduledoc """
@@ -21,12 +21,16 @@ defmodule VintageNet.WiFi.WPASupplicant do
 
   @impl true
   def init(args) do
-    {:ok, ll} = WPASupplicantLL.start_link(Keyword.get(args, :control_path))
+    control_path = Keyword.fetch!(args, :control_path)
+    ifname = Keyword.fetch!(args, :ifname)
+
+    {:ok, ll} = WPASupplicantLL.start_link(control_path)
     :ok = WPASupplicantLL.subscribe(ll)
 
     state = %{
       keep_alive_interval: Keyword.get(args, :keep_alive_interval, 60000),
-      ll: ll
+      ll: ll,
+      ifname: ifname
     }
 
     {:ok, state, {:continue, :continue}}
@@ -39,22 +43,85 @@ defmodule VintageNet.WiFi.WPASupplicant do
   end
 
   @impl true
+  def handle_call(:scan, _from, state) do
+    {:ok, "OK\n"} = WPASupplicantLL.control_request(state.ll, "SCAN")
+    {:reply, :ok, state}
+  end
+
+  @impl true
   def handle_info(:timeout, state) do
     {:ok, "PONG\n"} = WPASupplicantLL.control_request(state.ll, "PING")
     {:noreply, state, state.keep_alive_interval}
   end
 
-  def handle_info({VintageNet.WiFi.WPASupplicant, _priority, message}, state) do
-    new_state = handle_notification(message, state)
+  @impl true
+  def handle_info({VintageNet.WiFi.WPASupplicantLL, _priority, message}, state) do
+    new_state = handle_notification(String.trim_trailing(message), state)
     {:noreply, new_state, new_state.keep_alive_interval}
   end
 
-  defp handle_notification("CTRL-EVENT-BSS-ADDED " <> parameters, state) do
+  defp handle_notification("CTRL-EVENT-SCAN-RESULTS", state) do
+    # Collect all of the access points
+    access_points = all_bss(state.ll, 0, %{})
+
+    VintageNet.PropertyTable.put(
+      VintageNet,
+      ["interface", state.ifname, "access_points"],
+      access_points
+    )
+
     state
   end
 
+  # Ignored on purpose
+  defp handle_notification("CTRL-EVENT-SCAN-STARTED", state), do: state
+  defp handle_notification("CTRL-EVENT-BSS-ADDED " <> _rest, state), do: state
+  defp handle_notification("CTRL-EVENT-BSS-REMOVED " <> _rest, state), do: state
+  defp handle_notification("CTRL-EVENT-NETWORK-NOT-FOUND", state), do: state
+
   defp handle_notification(unknown_message, state) do
-    _ = Logger.info("WpaSupplicant ignoring #{inspect(unknown_message)}")
+    _ = Logger.info("WPASupplicant ignoring #{inspect(unknown_message)}")
     state
+  end
+
+  defp all_bss(ll, count, acc) do
+    {:ok, raw_response} = WPASupplicantLL.control_request(ll, "BSS #{count}")
+    response = WPASupplicantDecoder.decode_kv_response(raw_response)
+
+    if response == %{} do
+      acc
+    else
+      ap = %VintageNet.WiFi.AccessPoint{
+        bssid: response["bssid"],
+        frequency: response["freq"],
+        signal: response["level"],
+        flags: parse_flags(response["flags"]),
+        ssid: response["ssid"]
+      }
+
+      all_bss(ll, count + 1, Map.put(acc, ap.bssid, ap))
+    end
+  end
+
+  defp parse_flags(flags) do
+    flags
+    |> String.split(["]", "["], trim: true)
+    |> Enum.flat_map(&parse_flag/1)
+  end
+
+  defp parse_flag("WPA2-PSK-CCMP"), do: [:wpa2_psk_ccmp]
+  defp parse_flag("WPA2-EAP-CCMP"), do: [:wpa2_eap_ccmp]
+  defp parse_flag("WPA2-PSK-CCMP+TKIP"), do: [:wpa2_psk_ccmp_tkip]
+  defp parse_flag("WPA-PSK-CCMP+TKIP"), do: [:wpa_psk_ccmp_tkip]
+  defp parse_flag("IBSS"), do: [:ibss]
+  defp parse_flag("MESH"), do: [:mesh]
+  defp parse_flag("ESS"), do: [:ess]
+  defp parse_flag("P2P"), do: [:p2p]
+  defp parse_flag("WPS"), do: [:wps]
+  defp parse_flag("RSN--CCMP"), do: [:rsn_ccmp]
+
+  defp parse_flag(other) do
+    _ = Logger.warn("Ignoring unknown WiFi Access Point flag: #{other}")
+    []
   end
 end
--- test/vintage_net/wifi/wpa_supplicant_test.exs
+++ test/vintage_net/wifi/wpa_supplicant_test.exs
@@ -18,7 +18,7 @@ defmodule VintageNet.WiFi.WPASupplicantTest do
 
   test "attaches to wpa_supplicant", context do
     MockWPASupplicant.set_responses(context.mock, %{"ATTACH" => ["OK\n"]})
-    _ = start_supervised!({WPASupplicant, control_path: context.socket_path})
+    _ = start_supervised!({WPASupplicant, ifname: "wlan0", control_path: context.socket_path})
 
     Process.sleep(100)
     assert MockWPASupplicant.get_requests(context.mock) == ["ATTACH"]
@@ -29,12 +29,51 @@ defmodule VintageNet.WiFi.WPASupplicantTest do
 
     _ =
       start_supervised!(
-        {WPASupplicant, control_path: context.socket_path, keep_alive_interval: 10}
+        {WPASupplicant,
+         ifname: "wlan0", control_path: context.socket_path, keep_alive_interval: 10}
       )
 
     Process.sleep(100)
     requests = MockWPASupplicant.get_requests(context.mock)
     assert "ATTACH" in requests
     assert "PING" in requests
+  end
+
+  test "scan updates properties", context do
+    # TODO: Double check that this is what the real wpa_supplicant does
+    MockWPASupplicant.set_responses(context.mock, %{
+      "ATTACH" => "OK\n",
+      "PING" => "PONG\n",
+      "SCAN" => [
+        "OK\n",
+        "<2>CTRL-EVENT-SCAN-STARTED ",
+        "<2>CTRL-EVENT-BSS-ADDED 0 78:8a:20:87:7a:50",
+        "<2>CTRL-EVENT-SCAN-RESULTS ",
+        "<2>CTRL-EVENT-NETWORK-NOT-FOUND "
+      ],
+      "BSS 0" =>
+        "id=0\nbssid=78:8a:20:82:7a:50\nfreq=2437\nbeacon_int=100\ncapabilities=0x0431\nqual=0\nnoise=-89\nlevel=-71\ntsf=0000333220048880\nage=14\nie=0008426f7062654c414e010882848b968c1298240301062a01003204b048606c0b0504000a00002d1aac011bffffff00000000000000000001000000000000000000003d1606080c000000000000000000000000000000000000007f080000000000000040dd180050f2020101000003a4000027a4000042435e0062322f00dd0900037f01010000ff7fdd1300156d00010100010237e58106788a20867a5030140100000fac040100000fac040100000fac020000\nflags=[WPA2-PSK-CCMP][ESS]\nssid=TestLAN\nsnr=18\nest_throughput=48000\nupdate_idx=1\nbeacon_ie=0008426f7062654c414e010882848b968c1298240301060504010300002a01003204b048606c0b0504000a00002d1aac011bffffff00000000000000000001000000000000000000003d1606080c000000000000000000000000000000000000007f080000000000000040dd180050f2020101000003a4000027a4000042435e0062322f00dd0900037f01010000ff7fdd1300156d00010100010237e58106788a20867a5030140100000fac040100000fac040100000fac020000\n",
+      "BSS 1" => ""
+    })
+
+    supplicant =
+      start_supervised!({WPASupplicant, ifname: "test_wlan0", control_path: context.socket_path})
+
+    ap_property = ["interface", "test_wlan0", "access_points"]
+    VintageNet.PropertyTable.clear(VintageNet, ap_property)
+
+    VintageNet.subscribe(ap_property)
+    :ok = WPASupplicant.scan(supplicant)
+
+    assert_receive {VintageNet, ^ap_property, _old,
+                    %{
+                      "78:8a:20:82:7a:50" => %VintageNet.WiFi.AccessPoint{
+                        bssid: "78:8a:20:82:7a:50",
+                        flags: [:wpa2_psk_ccmp, :ess],
+                        frequency: 2437,
+                        signal: -71,
+                        ssid: "TestLAN"
+                      }
+                    }, _metadata}
   end
 end
