32da30fc81265ce080831bcb8aa990df683e26f1
Frank Hunleth <fhunleth@troodon-software.com>
2019-06-16 09:04:31 -0400

    Test multiple requests outstanding

--- test/vintage_net/wifi/wpa_supplicant_ll_test.exs
+++ test/vintage_net/wifi/wpa_supplicant_ll_test.exs
@@ -63,4 +63,25 @@ defmodule VintageNet.WiFi.WPASupplicantLLTest do
     assert_receive {VintageNet.WiFi.WPASupplicantLL, 1, "Notification"}
     assert MockWPASupplicant.get_requests(context.mock) == ["SCAN"]
   end
+
+  test "multiple requests outstanding", context do
+    ll = start_supervised!({WPASupplicantLL, context.socket_path})
+    :ok = WPASupplicantLL.subscribe(ll, self())
+
+    # Set up the responses so that REQUEST1 has to wait for REQUEST2
+    # to be sent before it gets a response
+    MockWPASupplicant.set_responses(context.mock, %{
+      "REQUEST1" => [],
+      "REQUEST2" => ["OK1", "OK2"]
+    })
+
+    spawn(fn ->
+      assert {:ok, "OK1"} = WPASupplicantLL.control_request(ll, "REQUEST1")
+    end)
+
+    # Make sure that REQUEST1 has time to be sent
+    Process.sleep(100)
+
+    assert {:ok, "OK2"} = WPASupplicantLL.control_request(ll, "REQUEST2")
+  end
 end
