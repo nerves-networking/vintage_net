36f43b5d285aed95e4dc5685b97b51028a8a6865
Frank Hunleth <fhunleth@troodon-software.com>
2019-06-26 22:52:12 -0400

    Improve udhcpc unit test

--- test/vintage_net/to_elixir_test.exs
+++ test/vintage_net/to_elixir_test.exs
@@ -1,6 +1,7 @@
 defmodule VintageNet.ToElixirTest do
   use ExUnit.Case
   import ExUnit.CaptureLog
+  alias VintageNetTest.CapturingUdhcpcHandler
 
   test "can send message from C" do
     assert capture_log(fn ->
@@ -11,11 +12,13 @@ defmodule VintageNet.ToElixirTest do
   end
 
   test "udhcpc handler notifies Elixir" do
-    log =
-      capture_log(fn ->
-        udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
+    udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
 
-        System.cmd(udhcpc_handler, ["deconfig"],
+    for op <- [:deconfig, :leasefail, :nak, :renew, :bound] do
+      CapturingUdhcpcHandler.clear()
+
+      {_, 0} =
+        System.cmd(udhcpc_handler, [to_string(op)],
           env: [
             {"subnet", "255.255.255.0"},
             {"router", "192.168.9.1"},
@@ -34,70 +37,86 @@ defmodule VintageNet.ToElixirTest do
           ]
         )
 
-        Process.sleep(250)
-      end)
+      Process.sleep(100)
 
-    assert log =~ "broadcast: \"192.168.9.255\""
-    assert log =~ "command: :deconfig"
-    assert log =~ "dns: [\"192.168.9.1\"]"
-    assert log =~ "domain: \"example.net\""
-    assert log =~ "interface: \"eth0\""
-    assert log =~ "ip: \"192.168.9.131\""
-    assert log =~ "lease: \"86400\""
-    assert log =~ "mask: \"24\""
-    assert log =~ "opt53: \"05\""
-    assert log =~ "opt58: \"0000a8c0\""
-    assert log =~ "opt59: \"00012750\""
-    assert log =~ "router: [\"192.168.9.1\"]"
-    assert log =~ "serverid: \"192.168.9.1\""
-    assert log =~ "siaddr: \"192.168.9.1\""
-    assert log =~ "subnet: \"255.255.255.0\""
+      [{ifname, reported_op, options}] = CapturingUdhcpcHandler.get()
+      assert reported_op == op
+      assert ifname == "eth0"
+      assert options[:dns] == ["192.168.9.1"]
+      assert options[:subnet] == "255.255.255.0"
+      assert options[:router] == ["192.168.9.1"]
+      assert options[:opt58] == "0000a8c0"
+      assert options[:opt59] == "00012750"
+      assert options[:domain] == "example.net"
+      assert options[:siaddr] == "192.168.9.1"
+      assert options[:serverid] == "192.168.9.1"
+      assert options[:broadcast] == "192.168.9.255"
+      assert options[:ip] == "192.168.9.131"
+      assert options[:mask] == "24"
+      assert options[:lease] == "86400"
+      assert options[:opt53] == "05"
+    end
   end
 
   test "udhcpc handler ignores uppercase fields" do
-    log =
-      capture_log(fn ->
-        udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
+    udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
+    CapturingUdhcpcHandler.clear()
 
-        System.cmd(udhcpc_handler, ["deconfig"],
-          env: [
-            {"interface", "eth0"},
-            {"broadcast", "broadcast"},
-            {"subnet", "subnet"},
-            {"router", "router"},
-            {"domain", "domain"},
-            {"PATH", "stuff"},
-            {"EMU", "beam"}
-          ]
-        )
+    {_, 0} =
+      System.cmd(udhcpc_handler, ["leasefail"],
+        env: [
+          {"interface", "eth0"},
+          {"broadcast", "broadcast"},
+          {"subnet", "subnet"},
+          {"router", "router"},
+          {"domain", "domain"},
+          {"PATH", "stuff"},
+          {"EMU", "beam"}
+        ]
+      )
 
-        Process.sleep(250)
-      end)
+    Process.sleep(100)
 
-    assert log =~ "broadcast: \"broadcast\""
-    assert log =~ "command: :deconfig"
-    assert log =~ "domain: \"domain\""
-    assert log =~ "interface: \"eth0\""
-    assert log =~ "router: [\"router\"]"
-    assert log =~ "subnet: \"subnet\""
+    [{ifname, reported_op, options}] = CapturingUdhcpcHandler.get()
+    assert reported_op == :leasefail
+    assert ifname == "eth0"
+    assert options[:broadcast] == "broadcast"
+    assert options[:domain] == "domain"
+    assert options[:router] == ["router"]
+    assert options[:subnet] == "subnet"
+    assert options[:PATH] == nil
+    assert options[:EMU] == nil
   end
 
   test "udhcpc handler handles multiple dns" do
-    log =
-      capture_log(fn ->
-        udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
+    udhcpc_handler = Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
+    CapturingUdhcpcHandler.clear()
 
-        System.cmd(udhcpc_handler, ["deconfig"],
-          env: [
-            {"interface", "eth0"},
-            {"ip", "ip"},
-            {"broadcast", "broadcast"},
-            {"subnet", "subnet"},
-            {"domain", "domain"},
-            {"dns", "1.1.1.1 2.2.2.2 3.3.3.3 4.4.4.4"},
-            {"message", "message"}
-          ]
-        )
+    {_, 0} =
+      System.cmd(udhcpc_handler, ["bound"],
+        env: [
+          {"interface", "eth0"},
+          {"ip", "ip"},
+          {"broadcast", "broadcast"},
+          {"subnet", "subnet"},
+          {"domain", "domain"},
+          {"dns", "1.1.1.1 2.2.2.2 3.3.3.3 4.4.4.4"},
+          {"message", "message"}
+        ]
+      )
+
+    Process.sleep(100)
+
+    [{ifname, reported_op, options}] = CapturingUdhcpcHandler.get()
+    assert reported_op == :bound
+    assert ifname == "eth0"
+    assert options[:ip] == "ip"
+    assert options[:broadcast] == "broadcast"
+    assert options[:subnet] == "subnet"
+    assert options[:domain] == "domain"
+    assert options[:dns] == ["1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4"]
+    assert options[:message] == "message"
+  end
 
         Process.sleep(250)
       end)
