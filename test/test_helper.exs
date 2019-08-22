2a2e5a18b1f2b629463933f84aeb3f8f452ae9ae
Frank Hunleth <fhunleth@troodon-software.com>
2019-08-22 16:48:26 -0400

    Disable parallel unit testing
    Given the "global" nature of network interfaces, it's possible to get
    failures when two tests interfere with each other on multicore machines.
    Disable parallelism to avoid figuring any of this out.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,4 +3,6 @@ File.rm_rf!("test/tmp")
 # VintageNet.InterfacesMonitor only works on Linux
 exclude = if :os.type() == {:unix, :linux}, do: [], else: [requires_interfaces_monitor: true]
 
-ExUnit.start(exclude: exclude)
+# Networking support has enough pieces that are singleton in nature
+# that parallel running of tests can't be done.
+ExUnit.start(exclude: exclude, max_cases: 1)
