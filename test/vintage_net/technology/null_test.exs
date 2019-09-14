c20fcd8711a0c4afd0cb645be3395092d58097e4
Frank Hunleth <fhunleth@troodon-software.com>
2019-09-14 09:11:17 -0400

    Add unit tests for the Null technology

---
+++ test/vintage_net/technology/null_test.exs
@@ -0,0 +1,27 @@
+defmodule VintageNet.Technology.NullTest do
+  use ExUnit.Case
+  alias VintageNet.Interface.RawConfig
+  alias VintageNet.Technology.Null
+
+  test "normalizing null" do
+    # Normalizing anything to Null should be Null
+    assert {:ok, %{type: VintageNet.Technology.Null} == Null.normalize(%{})}
+  end
+
+  test "converting to raw config" do
+    input = %{
+      type: VintageNet.Technology.Null
+    }
+
+    # Static IP support is not implemented. This is what is currently produced,
+    # but it is incomplete.
+    output = %RawConfig{
+      type: VintageNet.Technology.Null,
+      ifname: "eth0",
+      source_config: input,
+      require_interface: false
+    }
+
+    assert {:ok, output} == Null.to_raw_config("eth0", input, [])
+  end
+end
