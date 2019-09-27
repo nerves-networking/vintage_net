858db3907185f36a24d859984b21e2397e0c7b3d
Frank Hunleth <fhunleth@troodon-software.com>
2019-09-27 16:36:21 -0400

    Remove error about installing gcc+make for C errors
    The default error message seems to distract people from seeing the real
    issue when there's a C compile error.
    
    Specifically, this:
    
    ```
    <gcc error messages>
    ** (Mix) Could not compile with "make" (exit status: 2).
    You need to have gcc and make installed. If you are using
    Ubuntu or any other Debian-based system, install the packages
    "build-essential". Also install "erlang-dev" package if not
    included in your Erlang/OTP version. If you're on Fedora, run
    "dnf group install 'Development Tools'".
    ```
    
    turns into this:
    
    ```
    <gcc error messages>
    ** (Mix) Could not compile with "make" (exit status: 2).
    ```

--- mix.exs
+++ mix.exs
@@ -15,6 +15,7 @@ defmodule VintageNet.MixProject do
       compilers: [:elixir_make | Mix.compilers()],
       make_targets: ["all"],
       make_clean: ["clean"],
+      make_error_message: "",
       deps: deps(),
       dialyzer: dialyzer(),
       docs: docs(),
