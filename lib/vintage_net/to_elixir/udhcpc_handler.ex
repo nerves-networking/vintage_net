41c86b259dbbf962a92122133ad4678a88c8a2db
Frank Hunleth <fhunleth@troodon-software.com>
2020-01-15 22:21:58 -0500

    Cleanup udhcp* handler docs

--- lib/vintage_net/to_elixir/udhcpc_handler.ex
+++ lib/vintage_net/to_elixir/udhcpc_handler.ex
@@ -2,54 +2,57 @@ defmodule VintageNet.ToElixir.UdhcpcHandler do
   @moduledoc """
   A behaviour for handling notifications from udhcpc
 
-
-  # Example
+  ## Example
 
   ```elixir
   defmodule MyApp.UdhcpcHandler do
     @behaviour VintageNet.ToElixir.UdhcpcHandler
 
     @impl true
-    def deconfig(data) do
+    def deconfig(ifname, data) do
       ...
     end
   end
   ```
 
-  To have NervesHub invoke it, add the following to your `config.exs`:
+  To have VintageNet invoke it, add the following to your `config.exs`:
 
   ```elixir
   config :vintage_net, udhcpc_handler: MyApp.UdhcpcHandler
   ```
   """
 
-  @type ifname :: String.t()
   @type update_data :: map()
 
   @doc """
+  Deconfigure the specified interface
   """
-  @callback deconfig(ifname(), update_data()) :: :ok
+  @callback deconfig(VintageNet.ifname(), update_data()) :: :ok
 
   @doc """
+  Handle a failure to get a lease
   """
-  @callback leasefail(ifname(), update_data()) :: :ok
+  @callback leasefail(VintageNet.ifname(), update_data()) :: :ok
 
   @doc """
+  Handle a DHCP NAK
   """
-  @callback nak(ifname(), update_data()) :: :ok
+  @callback nak(VintageNet.ifname(), update_data()) :: :ok
 
   @doc """
+  Handle the renewal of a DHCP lease
   """
-  @callback renew(ifname(), update_data()) :: :ok
+  @callback renew(VintageNet.ifname(), update_data()) :: :ok
 
   @doc """
+  Handle an assignment from the DHCP server
   """
-  @callback bound(ifname(), update_data()) :: :ok
+  @callback bound(VintageNet.ifname(), update_data()) :: :ok
 
   @doc """
   Called internally by vintage_net to dispatch calls
   """
-  @spec dispatch(atom(), ifname(), update_data()) :: :ok
+  @spec dispatch(atom(), VintageNet.ifname(), update_data()) :: :ok
   def dispatch(function, ifname, update_data) do
     handler = Application.get_env(:vintage_net, :udhcpc_handler)
     apply(handler, function, [ifname, update_data])
