667bf22ece18fe50a29773c1cb959f5c03393e75
Frank Hunleth <fhunleth@troodon-software.com>
2020-04-03 22:30:46 -0400

    Bump deps

--- mix.exs
+++ mix.exs
@@ -96,7 +96,7 @@ defmodule VintageNet.MixProject do
       {:elixir_make, "~> 0.6", runtime: false},
       {:ex_doc, "~> 0.19", only: :docs, runtime: false},
       {:excoveralls, "~> 0.8", only: :test, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:muontrap, "~> 0.5.1"},
       {:gen_state_machine, "~> 2.0.0"},
       {:busybox, "~> 0.1.4", optional: true},
