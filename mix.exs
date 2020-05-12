2dd179160fdba1ae225c0461aa56b01352ab47e8
Frank Hunleth <fhunleth@troodon-software.com>
2020-05-12 10:30:49 -0400

    Add Changelog to hexdocs
    There's getting to be some good information in the Changelog and this
    makes it easier to find and link.

--- mix.exs
+++ mix.exs
@@ -116,10 +116,11 @@ defmodule VintageNet.MixProject do
 
   defp docs do
     [
-      extras: ["README.md", "docs/cookbook.md"],
+      extras: ["README.md", "docs/cookbook.md", "CHANGELOG.md"],
       main: "readme",
       source_ref: "v#{@version}",
-      source_url: @source_url
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
     ]
   end
 
