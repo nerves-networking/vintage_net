aea39e4ac1868462d9c855a5eaae3d19feb7fb5b
Frank Hunleth <fhunleth@troodon-software.com>
2020-09-14 08:40:06 -0400

    Bump ex_doc and fix doc generation warning

--- lib/vintage_net/interface/command_runner.ex
+++ lib/vintage_net/interface/command_runner.ex
@@ -7,7 +7,9 @@ defmodule VintageNet.Interface.CommandRunner do
 
   * `{:run, command, args}` - Run a system command
   * `{:run_ignore_exit, command, args}` - Same as `:run`, but without the exit status check
-  * `{:fun, fun}` - Run an function
+  * `{:fun, module, function_name, args}` - Run a function by MFA
+  * `{:fun, fun}` - Run a function. Using the MFA form is preferred since it's
+                    easier to verfiy in unit tests.
 
   CommandRunner also implements RawConfig's file creation and
   cleanup logic.
@@ -17,7 +19,9 @@ defmodule VintageNet.Interface.CommandRunner do
   alias VintageNet.Interface.{OutputLogger, RawConfig}
 
   @doc """
-  Run a list of commands
+  Run one or more commands
+
+  See the module docs for documentation on commands.
   """
   @spec run([RawConfig.command()] | RawConfig.command()) :: :ok | {:error, any()}
   def run([]), do: :ok
@@ -32,11 +36,6 @@ defmodule VintageNet.Interface.CommandRunner do
     end
   end
 
-  @doc """
-  Run a command
-
-  Non-zero exit status will return an error.
-  """
   def run({:run, command, args}) do
     case Command.muon_cmd(command, args,
            stderr_to_stdout: true,
@@ -51,9 +50,6 @@ defmodule VintageNet.Interface.CommandRunner do
     end
   end
 
-  @doc """
-  Run a command and ignore its exit code
-  """
   def run({:run_ignore_errors, command, args}) do
     _ =
       Command.muon_cmd(command, args,
@@ -64,14 +60,6 @@ defmodule VintageNet.Interface.CommandRunner do
     :ok
   end
 
-  @doc """
-  Call a function
-
-  In general, prefer the `module, function_name, args` form since it's easier to
-  check in unit tests.
-
-  Functions must return `:ok` or `{:error, reason}`.
-  """
   def run({:fun, module, function_name, args}) do
     apply(module, function_name, args)
   end
