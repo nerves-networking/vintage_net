5f44615bf1c4d1442b8bbf5898b1991561e75305
Frank Hunleth <fhunleth@troodon-software.com>
2021-04-06 14:39:37 -0400

    Support >1 internet host to check; refactor to be easier to test
    This adds the `:internet_host_list` configuration key and deprecates
    `:internet_host`. The new key, `:internet_host_list`, takes a list of IP
    address, port tuples and attempts to connect to each one of them in
    succession to establish whether the internet is available on a network
    interface..
    
    This fixes #77. It also lets you specify the TCP port number to check so
    that it's possible to test a connection to the service being run. As
    before, once the connection is made, it's closed since that's good
    enough for determining connectivity.
    
    As part of this change, there was quite a bit of untested code and the
    addition of the new code made me worried that it might break easily.
    This pulls much of the side effect-free code out so that it can be
    tested.
    
    Nothing big was found when adding tests. The main changes were to
    bulletproof functions being called with values that could theoretically
    lead to harmless, but unintended behavior. For example, if the interface
    was removed and this was caught when trying to connect, then a retry
    timeout would be set even though the intention was for the interface to
    be marked disconnected and have no timeout.

--- lib/vintage_net/interface/internet_connectivity_checker.ex
+++ lib/vintage_net/interface/internet_connectivity_checker.ex
@@ -2,7 +2,7 @@ defmodule VintageNet.Interface.InternetConnectivityChecker do
   use GenServer
   require Logger
 
-  alias VintageNet.Interface.InternetTester
+  alias VintageNet.Interface.{Classification, InternetTester}
   alias VintageNet.{PropertyTable, RouteManager}
 
   @moduledoc """
@@ -17,6 +17,14 @@ defmodule VintageNet.Interface.InternetConnectivityChecker do
   @max_interval 30_000
   @max_fails_in_a_row 3
 
+  @typep state() :: %{
+           ifname: VintageNet.ifname(),
+           hosts: [{VintageNet.any_ip_address(), non_neg_integer()}],
+           strikes: non_neg_integer(),
+           interval: non_neg_integer(),
+           connectivity: Classification.connection_status()
+         }
+
   @doc """
   Start the connectivity checker GenServer
   """
@@ -27,14 +35,20 @@ defmodule VintageNet.Interface.InternetConnectivityChecker do
 
   @impl GenServer
   def init(ifname) do
-    # Handle GenServer restarts when internet-connected
-    initial_strikes =
-      case VintageNet.get(["interface", ifname, "connection"]) do
-        :internet -> 0
-        _ -> @max_fails_in_a_row
-      end
+    # Handle GenServer restarts when internet-connected. If internet connected,
+    # then start the strike count over at zero since who knows where things were
+    # at and that way a first strike doesn't bounce the status.
+    connectivity = VintageNet.get(["interface", ifname, "connection"])
+    initial_strikes = if connectivity == :internet, do: 0, else: @max_fails_in_a_row
 
-    state = %{ifname: ifname, strikes: initial_strikes, interval: @min_interval}
+    state = %{
+      ifname: ifname,
+      hosts: get_internet_host_list(),
+      strikes: initial_strikes,
+      interval: @min_interval,
+      connectivity: connectivity
+    }
+
     {:ok, state, {:continue, :continue}}
   end
 
@@ -42,18 +56,14 @@ defmodule VintageNet.Interface.InternetConnectivityChecker do
   def handle_continue(:continue, %{ifname: ifname} = state) do
     VintageNet.subscribe(lower_up_property(ifname))
 
-    case VintageNet.get(lower_up_property(ifname)) do
-      true ->
-        new_state = check_connectivity(state)
+    new_state =
+      if VintageNet.get(lower_up_property(ifname)) do
+        check_connectivity(state)
+      else
+        state |> ifdown() |> report_connectivity()
+      end
 
-        {:noreply, new_state, new_state.interval}
-
-      _not_true ->
-        # If the physical layer isn't up, don't start polling until
-        # we're notified that it is available.
-        set_connectivity(ifname, :disconnected)
-        {:noreply, state}
-    end
+    {:noreply, new_state, new_state.interval}
   end
 
   @impl GenServer
@@ -67,92 +77,176 @@ defmodule VintageNet.Interface.InternetConnectivityChecker do
         {VintageNet, ["interface", ifname, "lower_up"], _old_value, false, _meta},
         %{ifname: ifname} = state
       ) do
-    # Physical layer is down. We're definitely disconnected, so skip right to it and
-    # don't poll until the lower_up changes
-    set_connectivity(ifname, :disconnected)
-    {:noreply, state}
+    new_state = state |> ifdown() |> report_connectivity()
+
+    {:noreply, new_state}
   end
 
   def handle_info(
         {VintageNet, ["interface", ifname, "lower_up"], _old_value, true, _meta},
         %{ifname: ifname} = state
       ) do
-    # Physical layer is up. Optimistically assume that the LAN is accessible and
-    # start polling again after a short delay
-    set_connectivity(ifname, :lan)
+    new_state = state |> ifup() |> report_connectivity()
 
-    new_state = %{state | interval: @min_interval}
     {:noreply, new_state, @min_interval}
   end
 
   def handle_info(
-        {VintageNet, ["interface", ifname, "lower_up"], old_value, nil, _meta},
+        {VintageNet, ["interface", ifname, "lower_up"], _old_value, nil, _meta},
         %{ifname: ifname} = state
       ) do
     # The interface was completely removed!
-    if old_value, do: set_connectivity(ifname, :disconnected)
-    {:noreply, state}
+    new_state = state |> ifdown() |> report_connectivity()
+    {:noreply, new_state}
   end
 
-  defp check_connectivity(%{ifname: ifname, strikes: strikes, interval: interval} = state) do
-    {connectivity, new_strikes} =
-      case InternetTester.ping(ifname) do
-        :ok ->
-          # Success - reset the number of strikes to stay in Internet mode
-          # even if there are hiccups.
-          {:internet, 0}
-
-        {:error, :if_not_found} ->
-          {:disconnected, @max_fails_in_a_row}
-
-        {:error, :no_ipv4_address} ->
-          {:lan, @max_fails_in_a_row}
-
-        {:error, reason} ->
-          if strikes < @max_fails_in_a_row do
-            Logger.debug(
-              "#{ifname}: Internet test failed (#{inspect(reason)}: #{strikes + 1}/#{
-                @max_fails_in_a_row
-              } strikes"
-            )
-
-            {:internet, strikes + 1}
-          else
-            Logger.debug("#{ifname}: Internet test failed: (#{inspect(reason)})")
-            {:lan, @max_fails_in_a_row}
-          end
-      end
-
-    new_state = %{
-      state
-      | strikes: new_strikes,
-        interval: next_interval(connectivity, interval, strikes)
-    }
-
-    set_connectivity(ifname, connectivity)
-
-    new_state
+  defp ifdown(state) do
+    # Physical layer is down. Don't poll for connectivity since it won't happen.
+    %{state | connectivity: :disconnected, interval: :infinity}
   end
 
-  defp set_connectivity(ifname, connectivity) do
+  defp ifup(state) do
+    # Physical layer is up. Optimistically assume that the LAN is accessible and
+    # start polling again after a short delay
+    %{state | connectivity: :lan, interval: @min_interval}
+  end
+
+  defp check_connectivity(state) do
+    ping_result = InternetTester.ping(state.ifname, hd(state.hosts))
+
+    state
+    |> update_state_from_ping(ping_result)
+    |> report_connectivity()
+    |> compute_next_interval()
+  end
+
+  # Public for unit test purposes
+  @doc false
+  @spec update_state_from_ping(state(), :ok | {:error, InternetTester.ping_error_reason()}) ::
+          state()
+  def update_state_from_ping(state, :ok) do
+    # Success - reset the number of strikes to stay in Internet mode
+    # even if there are hiccups.
+    %{state | connectivity: :internet, strikes: 0}
+  end
+
+  def update_state_from_ping(state, {:error, :if_not_found}) do
+    %{state | connectivity: :disconnected, strikes: @max_fails_in_a_row}
+  end
+
+  def update_state_from_ping(state, {:error, :no_ipv4_address}) do
+    %{state | connectivity: :lan, strikes: @max_fails_in_a_row}
+  end
+
+  def update_state_from_ping(%{connectivity: :internet} = state, {:error, reason}) do
+    strikes = state.strikes + 1
+
+    if strikes < @max_fails_in_a_row do
+      Logger.debug(
+        "#{state.ifname}: Internet check failed to #{inspect(hd(state.hosts))} (#{inspect(reason)}): #{
+          strikes
+        }/#{@max_fails_in_a_row} strikes"
+      )
+
+      %{state | connectivity: :internet, strikes: strikes, hosts: rotate_list(state.hosts)}
+    else
+      Logger.debug("#{state.ifname}: Internet unreachable: (#{inspect(reason)})")
+      %{state | connectivity: :lan, strikes: @max_fails_in_a_row}
+    end
+  end
+
+  def update_state_from_ping(state, {:error, _reason}) do
+    # Final case where the internet wasn't reachable and it wasn't reachable before this.
+    # Rotate the hosts to check and maybe we'll get lucky next time.
+    %{state | hosts: rotate_list(state.hosts)}
+  end
+
+  defp report_connectivity(%{ifname: ifname, connectivity: connectivity} = state) do
+    # It's desirable to set these even if redundant since the checks in this
+    # modules are authoritative. I.e., the internet isn't connected unless we
+    # declare it detected. Other modules can reset the connection to :lan
+    # if, for example, a new IP address gets set by DHCP. The following functions
+    # will optimize out redundant calls if they really are redundant.
     RouteManager.set_connection_status(ifname, connectivity)
     PropertyTable.put(VintageNet, ["interface", ifname, "connection"], connectivity)
+    state
   end
 
   defp lower_up_property(ifname) do
     ["interface", ifname, "lower_up"]
   end
 
+  # Public for unit test purposes
+  @doc false
+  @spec compute_next_interval(state()) :: state()
+  def compute_next_interval(state) do
+    %{state | interval: next_interval(state.connectivity, state.interval, state.strikes)}
+  end
+
+  # Public for unit test purposes
+  @doc false
+  @spec next_interval(Classification.connection_status(), non_neg_integer(), non_neg_integer()) ::
+          non_neg_integer()
+  def next_interval(connection, interval, strikes)
+
   # If pings work, then wait the max interval before checking again
-  defp next_interval(:internet, _interval, 0), do: @max_interval
+  def next_interval(:internet, _interval, 0), do: @max_interval
 
   # If a ping fails, retry, but don't wait as long as when everything is working
-  defp next_interval(:internet, _interval, strikes) do
+  def next_interval(:internet, _interval, strikes) do
     max(@min_interval, div(@max_interval, strikes + 1))
   end
 
   # Back off of checks if they're not working
-  defp next_interval(_not_internet, interval, _strikes) do
+  def next_interval(:lan, interval, _strikes) do
     min(interval * 2, @max_interval)
+  end
+
+  # Wait for interface up notification before polling again
+  def next_interval(:disconnected, _interval, _strikes), do: :infinity
+
+  # Rotate a list left
+  @doc false
+  @spec rotate_list(list()) :: list()
+  def rotate_list([]), do: []
+  def rotate_list(hosts), do: tl(hosts) ++ [hd(hosts)]
+
+  defp get_internet_host_list() do
+    hosts = legacy_internet_host() ++ Application.get_env(:vintage_net, :internet_host_list)
+    good_hosts = Enum.flat_map(hosts, &normalize_internet_host/1)
+
+    if good_hosts == [] do
+      Logger.warn("VintageNet: `:internet_host_list` is invalid. Using defaults")
+      [{1, 1, 1, 1}, 80]
+    else
+      good_hosts
+    end
+  end
+
+  defp legacy_internet_host() do
+    case Application.get_env(:vintage_net, :internet_host) do
+      nil ->
+        []
+
+      host ->
+        Logger.warn(
+          "VintageNet: Legacy :internet_host key is in use. Please change this to `internet_host_list: [{#{
+            inspect(host)
+          }, 80}]."
+        )
+
+        [{host, 80}]
+    end
+  end
+
+  defp normalize_internet_host({host, port}) when port > 0 and port < 65536 do
+    case VintageNet.IP.ip_to_tuple(host) do
+      {:ok, host_as_tuple} -> [{host_as_tuple, port}]
+      _anything_else -> []
+    end
+  end
+
+  defp normalize_internet_host(other) do
+    Logger.warn("VintageNet: Dropping invalid Internet destination (#{inspect(other)})")
   end
 end
