fb15b451c535b6b00522165064b1f743268bbc62
Frank Hunleth <fhunleth@troodon-software.com>
2021-11-18 19:18:30 -0500

    Make code more consistent with more credo

--- .credo.exs
+++ .credo.exs
@@ -1,4 +1,4 @@
-# config/.credo.exs
+# .credo.exs
 %{
   configs: [
     %{
@@ -7,7 +7,9 @@
         {Credo.Check.Refactor.MapInto, false},
         {Credo.Check.Warning.LazyLogging, false},
         {Credo.Check.Readability.LargeNumbers, only_greater_than: 86400},
-        {Credo.Check.Readability.ParenthesesOnZeroArityDefs, parens: true}
+        {Credo.Check.Readability.ParenthesesOnZeroArityDefs, parens: true},
+        # {Credo.Check.Readability.Specs, []},
+        {Credo.Check.Readability.StrictModuleLayout, tags: []}
       ]
     }
   ]
