df7ee2273266bc2230e13506c0727dd162fc4e9e
Frank Hunleth <fhunleth@troodon-software.com>
2021-12-15 19:12:40 -0500

    Support pinging IPv6 addresses

--- test/vintage_net/connectivity/tcp_ping_test.exs
+++ test/vintage_net/connectivity/tcp_ping_test.exs
@@ -4,12 +4,29 @@ defmodule VintageNet.Connectivity.TCPPingTest do
   alias VintageNet.Connectivity.TCPPing
   alias VintageNetTest.Utils
 
-  test "ping known hosts" do
+  test "ping IPv4 known hosts" do
     ifname = Utils.get_ifname_for_tests()
 
     assert TCPPing.ping(ifname, {"127.0.0.1", 80}) == :ok
-    assert TCPPing.ping(ifname, {"1.1.1.1", 80}) == :ok
-    assert TCPPing.ping(ifname, {{1, 1, 1, 1}, 80}) == :ok
+    assert TCPPing.ping(ifname, {"1.1.1.1", 53}) == :ok
+  end
+
+  # If this fails and your LAN doesn't support IPv6, run "mix test --exclude requires_ipv6"
+  @tag :requires_ipv6
+  test "ping IPv6 known hosts" do
+    ifname = Utils.get_ifname_for_tests()
+
+    assert TCPPing.ping(ifname, {"::1", 80}) == :ok
+    assert TCPPing.ping(ifname, {"2606:4700:4700::1111", 53}) == :ok
+  end
+
+  test "ping internet_host_list" do
+    ifname = Utils.get_ifname_for_tests()
+
+    # While these won't work for everyone, they should work on CI
+    for host_port <- Application.fetch_env!(:vintage_net, :internet_host_list) do
+      assert TCPPing.ping(ifname, host_port) == :ok
+    end
   end
 
   test "ping IP addresses that shouldn't work" do
