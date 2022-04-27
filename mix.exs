e62f02f1907d4c74dfe5c1f670fcbd926c258c7e
Frank Hunleth <fhunleth@troodon-software.com>
2022-04-27 09:16:46 -0400

    Bump minimum Elixir version to 1.11
    This is required now since the `property_table` dependency requires
    Elixir 1.11 or later. Plus, it's time.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule VintageNet.MixProject do
     [
       app: :vintage_net,
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
       start_permanent: Mix.env() == :prod,
